8213f294fcf2e7940ddba4cd17fc921871c37dcd
bleezie <myfriendiscubey@gmail.com>
2025-09-07 02:08:33 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
-# --- basics ---
-echo "--- installing basic packages... ---"
+# --- install packages ---
+echo "--- installing basic packages ---"
 sudo pacman -S --needed --noconfirm hyprland thunar kitty rofi swww python-pywal fish git neofetch waybar
 echo "--- done installing basic packages ---"
 
 # --- clone dotfiles ---
-echo "--- cloning dotfiles into .config---"
+echo "--- cloning dotfiles into ~/dotfiles ---"
 git clone https://github.com/bIeezie/bleezies-dotfiles.git ~/dotfiles
 
-# --- copy dotfiles into .config ---
+# --- copy dotfiles into ~/.config ---
+echo "--- copying config files ---"
 cp -r ~/dotfiles/config/fish ~/.config/
 cp -r ~/dotfiles/config/hypr ~/.config/
 cp -r ~/dotfiles/config/kitty ~/.config/
 cp -r ~/dotfiles/config/neofetch ~/.config/
 cp -r ~/dotfiles/config/rofi ~/.config/
 cp -r ~/dotfiles/config/waybar ~/.config/
-echo "--- done cloning files ---"
 
 # --- make scripts executable ---
-chmod +x ~/scripts/*.sh
+echo "--- making scripts executable ---"
+chmod +x ~/dotfiles/scripts/*.sh
 
 echo "--- finished ---"
