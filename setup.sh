c3e14512d806e434f2094f53cd9b849cf901fcea
bleezie <myfriendiscubey@gmail.com>
2025-09-07 03:07:09 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,13 +3,29 @@
 # --- install packages ---
 echo "--- installing basic packages ---"
 sudo pacman -S --needed --noconfirm hyprland
+pacman -Q hyprland
+echo "-hyprland installed-"
 sudo pacman -S --needed --noconfirm thunar
+pacman -Q thunar
+echo "-thunar installed-"
 sudo pacman -S --needed --noconfirm kitty
+pacman -Q kitty
+echo "-kitty installed-"
 sudo pacman -S --needed --noconfirm rofi
+pacman -Q rofi
+echo "-rofi installed-"
 sudo pacman -S --needed --noconfirm swww
+pacman -Q swww
+echo "-swww installed-"
 sudo pacman -S --needed --noconfirm python-pywal
+pacman -Q python-pywal
+echo "-pywal installed-"
 sudo pacman -S --needed --noconfirm fish
+pacman -Q fish
+echo "-fish installed-"
 sudo pacman -S --needed --noconfirm waybar
+pacman -Q waybar
+echo "-waybar installed-"
 echo "--- done installing basic packages ---"
 
 # --- clone dotfiles ---
